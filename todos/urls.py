13d81c59b69858d0910d3fbb12980b39ec8f2acb
Leslie Alldridge <leslie.alldridge@gmail.com>
2019-01-16 19:13:46 +1300

    delete operational
    delete operational

--- todos/urls.py
+++ todos/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url('^$', views.index, name="index"),
     url('^details/(?P<id>\w{0,50})/$', views.details),
     url('^add', views.add, name='add'),
-    url('^delete', views.delete, name='delete'),
-    url('^delete/(?P<id>\w{0,50})/$', views.delete, name='delete')
+    
+    url('^todos/delete/(?P<id>\w{0,50})$', views.delete, name='delete')
 ]
